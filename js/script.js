d5932b7018ab5870f2f15ae214b4e9011af9be10
DM-smile <86293366+DM-smile@users.noreply.github.com>
2024-12-02 19:48:08 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,66 @@
+// burger button
+const icon = document.querySelector(".burger-button");
+icon.addEventListener("click", function () {
+   document.documentElement.classList.toggle("burger-open");
+});
+
+// swiper
+const swiperFirst = new Swiper('.popular-first__body', {
+   loop: true,
+   spaceBetween: 30,
+   grabCursor: true,
+   speed: 800,
+   autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
+   },
+
+   keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+      pageUpDown: true,
+   },
+   breakpoints: {
+      375: {
+         slidesPerView: 2,
+      },
+      768: {
+         slidesPerView: 3,
+      },
+      992: {
+         slidesPerView: 4,
+      }
+   },
+});
+const swiperSecond = new Swiper('.popular-second__body', {
+   loop: true,
+   grabCursor: true,
+   spaceBetween: 30,
+   speed: 800,
+   keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+      pageUpDown: true,
+   },
+   navigation: {
+      nextEl: '.popular-second__navigation-next',
+      prevEl: '.popular-second__navigation-prev'
+   },
+   breakpoints: {
+      375: {
+         slidesPerView: 1,
+      },
+      768: {
+         slidesPerView: 2,
+      },
+      992: {
+         slidesPerView: 3,
+      },
+   },
+   autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
+   }
+})
